a16a53ad84e00f12c0cdb33e7ba2a4ddf7d6f6c0
Willberg <hszhongtao@163.com>
2016-08-29 16:42:31 +0800

    pc,手机url修改跳转

--- home/Lib/Action/CommonAction.class.php
+++ home/Lib/Action/CommonAction.class.php
@@ -2,7 +2,41 @@
 	class CommonAction extends Action{
 		protected $common_classs = null;
 
+		protected function myIsRedict(){
+			$client = $_SERVER['HTTP_USER_AGENT'];
+			#$url = $_SERVER['HTTP_HOST'];
+			$url = $_SERVER['REQUEST_URI'];
+			$is_include_m = strpos($url,'m');
+			$clientkeywords = array('iphone','mobile','ipod','ipad','android','symbianos','windows phone','phone');
+			// 从HTTP_USER_AGENT中查找手机浏览器的关键字
+			$is_mobile = preg_match("/(".implode('|', $clientkeywords).")/i", strtolower($client));
+			$flag = false;
+			if (!$is_mobile && $is_include_m){
+				$flag = true;
+			}
+
+			if($is_mobile && !$is_include_m){
+				$flag = true;
+			}
+			return $flag;
+		}
+
+		protected function myRediectUrl(){
+			$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+			if(strpos($url,'m')){
+				$url = preg_replace("/(m\.)|(\/m)/","",$url);
+			}else{
+				$url = $url.'m';
+				#$url = 'm.'.$url;
+			}
+			return "http://".$url;
+		}
+
 		public function _initialize(){
+			if($this->myIsRedict()){
+				redirect($this->myRediectUrl());
+			}
+
 			//网站信息
 			$s=M('Site');
 			$siteinfo=$s->find(1);
