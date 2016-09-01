45415c281805c35de4e1449d7b84453161db6523
Willberg <hszhongtao@163.com>
2016-09-01 12:30:39 +0800

    手机,pc url跳转

--- home/Lib/Action/CommonAction.class.php
+++ home/Lib/Action/CommonAction.class.php
@@ -6,8 +6,8 @@
 			$client = $_SERVER['HTTP_USER_AGENT'];
 			#$url = $_SERVER['HTTP_HOST'];
 			$url = $_SERVER['REQUEST_URI'];
-			$is_include_m = strpos($url,'/m');
-			#$is_include_m = strpos($url,'m.');
+			#$is_include_m = strpos($url,'/m');
+			$is_include_m = strpos($url,'m.');
 			$clientkeywords = array('iphone','mobile','ipod','ipad','android','symbianos','windows phone','phone');
 			// 从HTTP_USER_AGENT中查找手机浏览器的关键字
 			$is_mobile = preg_match("/(".implode('|', $clientkeywords).")/i", strtolower($client));
@@ -24,12 +24,13 @@
 
 		protected function myRediectUrl(){
 			$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-			if(strpos($url,'m')){
-				$url = preg_replace("/(m\.)|(\/m)/","",$url);
+			if(strpos($url,'m.')){
+				$url = preg_replace("/(m\.)/","",$url);
+				#$url = preg_replace("/(\/m)/","",$url);
 			}else{
 				#$url = $url.'m';
-				#$url = 'm.'.$url;
-				$url = str_replace('localhost/novel', 'localhost/novel/m', $url);
+				$url = 'm.'.$url;
+				#$url = str_replace('localhost/novel', 'localhost/novel/m', $url);
 			}
 			return "http://".$url;
 		}
