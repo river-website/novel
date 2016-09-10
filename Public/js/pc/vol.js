16e84620fad81d669ce910039ef871b78453b484
Willberg <hszhongtao@163.com>
2016-09-10 16:53:51 +0800

    pc页面js文件夹

---
+++ Public/js/pc/vol.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bill on 16/9/10.
+ */
+
+//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+$(function () {
+    $(window).scroll(function(){
+        if ($(window).scrollTop()>300){
+            $("#back-to-top").fadeIn(1500);
+        }
+        else
+        {
+            $("#back-to-top").fadeOut(1500);
+        }
+    });
+
+    //当点击跳转链接后，回到页面顶部位置
+    $("#back-to-top").click(function(){
+        $('body,html').animate({scrollTop:0},1000);
+        return false;
+    });
+});
